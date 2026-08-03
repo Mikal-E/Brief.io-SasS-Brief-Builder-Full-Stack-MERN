b04dc22ffb54f7d491773eee72944a0e2cadc29f
Mikal-E <melamin101@gmail.com>
2026-08-03 12:44:40 -0400

    Finished Hero section in Home.jsx and also styling of hero in Home.css.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,29 @@
+import "./Home.css"
+import landscape from "../../assets/landscape.jpg"
+
 function HomePage() {
 
     return (
 
         <div>
 
-            <h1>Home Page</h1>
+            <section className="hero" style={{ backgroundImage: `url(${landscape})` }}>
+
+                <div className="hero-content">
+
+                    <h1 className="hero-headline">Ra que verist et
+                    <br />
+                    omniet a ducipitis di</h1>
+                    <p className="hero-subhead">Aped eos solupta tureped ut explaut qui
+                    <br />
+                    debis sum autest parum et et offic</p>
+                    <button className="hero-cta">Button CTA</button>
+
+                </div>
+
+            </section>
+
+            <section></section>
 
         </div>
 
