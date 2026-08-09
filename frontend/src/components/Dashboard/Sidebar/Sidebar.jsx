e9439385c71ed761eae815780b33ffe5cf56cebf
Mikal-E <melamin101@gmail.com>
2026-08-08 21:58:33 -0400

    Made small tweaks to the Sidebar and TopBar in the DashBoard. Changed placeholder <p> Log Out link in Sidebar footer to a <Link>, and appled { useNavigate } to redirect a logged in user to / when logging out.

--- frontend/src/components/Dashboard/Sidebar/Sidebar.jsx
+++ frontend/src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -1,8 +1,18 @@
 import { NavLink } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import briefioLogo from "../../../assets/brief.io-logo-placeholder.png"
 import "./Sidebar.css"
 
 function Sidebar() {
+
+    const navigate = useNavigate()
+
+    function handleLogout() {
+
+        localStorage.removeItem("token")
+        navigate("/")
+        
+    }
 
     return (
 
@@ -65,7 +75,7 @@ function Sidebar() {
             <div className="dashnav-footer">
 
                 <p><i className="fa-solid fa-gear"></i>Settings</p>
-                <p><i className="fa-solid fa-right-from-bracket"></i>Log Out</p>
+                <button onClick={handleLogout} className="dashnav-logout"><i className="fa-solid fa-right-from-bracket"></i>Log Out</button>
 
             </div>
 
