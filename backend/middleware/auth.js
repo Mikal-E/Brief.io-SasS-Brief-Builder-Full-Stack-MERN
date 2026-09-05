45da71810e1c57a599a2a67219143a779165a6c0
Mikal-E <melamin101@gmail.com>
2026-09-05 17:13:23 -0400

    Imported /models/User.js into auth.js, added user's email into req.user, and made the function async.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,8 +1,9 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
 /* Auth middleware to verify the JWT and set req.user. The front end sends Authorization: Bearer <token> on requests to protected routes. Sets req.user.id for use in downstream routes. */
 
-const requireAuth = (req, res, next) => {
+const requireAuth = async (req, res, next) => {
 
     const authHeader = req.headers.authorization;
 
@@ -15,11 +16,15 @@ const requireAuth = (req, res, next) => {
     const token = authHeader.split(" ")[1];
 
     try {
-
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = { id: decoded.id };
-        next();
+        const user = await User.findById(decoded.id);
 
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
+        req.user = { id: user._id, email: user.email };
+        next();
     } catch (error) {
 
         return res.status(401).json({ message: "Your session has expired. Please log in again." });
